1feb0c1f2e9b6df584b4d91604751c60916725f8
DestinySolomon <deesystems247@gmail.com>
2026-08-11 17:29:34 +0100

    added to config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/myportfolio",
   define: {
     __BASE_PATH__: JSON.stringify('/'),
     __IS_PREVIEW__: JSON.stringify(false),
